873647bb834a920a88641a418d93961650dcec09
Libb521 <eotieno39@yahoo.com>
2019-12-06 18:02:15 +0300

    reworked on the script

--- Delani studios/js/script.js
+++ Delani studios/js/script.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
+    $('.des p').hide();
     $('.des').click(function () {
-        $('img').toggle();
         $('.des p').toggle();
+        $('.des img').toggle();
+    });
+
+    $('.dev p').hide();
+    $('.dev').click(function () {
+        $('.dev p').toggle();
+        $('.dev img').toggle();
+    });
+
+    $('.prod p').hide();
+    $('.prod').click(function () {
+        $('.prod p').toggle();
+        $('.prod img').toggle();
     });
 });
-
 
